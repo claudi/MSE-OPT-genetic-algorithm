330fed6255c67f28be6d07b04cd9378dd8ff93ae
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2021-12-31 12:26:38 +0100

    Remove extra semicolon

--- src/genotype.c
+++ src/genotype.c
@@ -4,7 +4,7 @@
 Phenotype genoype_to_phenotype(const Genotype g) {
     const double phi = g.phi * ((0.35 + 100.0) / ((double) (1UL << 34) - 1)) - 100.0;
     const double lambda = g.lambda * (30000.0 / ((double) (1UL << 25) - 1));
-    const double mu = g.mu * (20.0 / ((double) (1UL << 25) - 1));;
+    const double mu = g.mu * (20.0 / ((double) (1UL << 25) - 1));
     const double sigma = g.sigma * (1000.0 / ((double) (1UL << 17) - 1));
     const double delta = g.delta * (25000.0 / ((double) (1UL << 15) - 1));
 
