12ba45c9dcb10d5ffd7cd515867ee07c23067ac4
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-01-01 17:39:19 +0100

    Add functions `individual_crossover` and `mutate_individual`

--- src/genetic-algorithm.c
+++ src/genetic-algorithm.c
@@ -1,5 +1,16 @@
 #include "genetic-algorithm.h"
+#include "genotype.h"
 
 static Individual select_individual_with_replacement(const Individual *individuals, const unsigned n_individuals);
-static Individual individual_crossover(const Individual g1, const Individual g2);
-static Individual mutate_individual(const Individual g);
+
+/* Mix and match  genotypes of two individuals to form two children genotypes.
+ */
+static void individual_crossover(const Individual p1, const Individual p2, Individual *const c1, Individual *const c2) {
+    genotype_crossover(p1.genotype, p2.genotype, &(c1->genotype), &(c2->genotype));
+}
+
+/* Randomly mutate bits of a genotype of an individual.
+ */
+static void mutate_individual(Individual *const individual) {
+    mutate_genotype(&(individual->genotype));
+}
