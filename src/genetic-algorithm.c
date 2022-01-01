c4e13b2254526f4945ae9243f3620f53442a9cff
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-01-01 18:23:15 +0100

    Add individual selection methods

--- src/genetic-algorithm.c
+++ src/genetic-algorithm.c
@@ -1,7 +1,39 @@
 #include "genetic-algorithm.h"
 #include "genotype.h"
+#include "randombits.h"
 
-static Individual select_individual_with_replacement(const Individual *individuals, const unsigned n_individuals);
+/* Get a random individual from the population (with replacement).
+ *
+ * Disregards their fitness or genotype.
+ */
+static Individual get_random_individual(const Individual *individuals, const unsigned n_individuals) {
+    const unsigned index = uniform() * n_individuals;
+
+    return individuals[index];
+}
+
+/* Select the best individual from a tournament of `size` random individuals
+ * from the population.
+ */
+static Individual tournament_selection(const Individual *individuals, const unsigned n_individuals, const unsigned char size) {
+    Individual best = get_random_individual(individuals, n_individuals);
+
+    for(unsigned char iter = 1; iter < size; iter++) {
+        Individual tmp = get_random_individual(individuals, n_individuals);
+
+        if(tmp.fitness > best.fitness) {
+            best = tmp;
+        }
+    }
+
+    return best;
+}
+
+/* Returns a random individual from the population.
+ */
+static Individual select_individual_with_replacement(const Individual *individuals, const unsigned n_individuals) {
+    return get_random_individual(individuals, n_individuals);
+}
 
 /* Mix and match  genotypes of two individuals to form two children genotypes.
  */
