68621163f44a12582c22d0e837b1b9731cd617b3
Claudi Lleyda Moltó <claudilleyda@gmail.com>
2022-01-01 13:19:24 +0100

    Implement `genotype_crossover` and `mutate_genotype`

--- src/genotype.c
+++ src/genotype.c
@@ -27,3 +27,72 @@ Genotype get_random_genotype() {
         .delta = random_US_length(delta_length),
     };
 }
+
+void one_point_crossover(const Genotype p1, const Genotype p2, Genotype *const c1, Genotype *const c2) {
+    unsigned char d = uniform() * (phi_length - 1) + 1;
+    unsigned char di = phi_length - d;
+    c1->phi = ((p1.phi >> d) << d) | ((p2.phi << di) >> di);
+    c2->phi = ((p2.phi >> d) << d) | ((p1.phi << di) >> di);
+
+    d = uniform() * (lambda_length - 1) + 1;
+    di = phi_length - d;
+    c1->lambda = ((p1.lambda >> d) << d) | ((p2.lambda << di) >> di);
+    c2->lambda = ((p2.lambda >> d) << d) | ((p1.lambda << di) >> di);
+
+    d = uniform() * (mu_length - 1) + 1;
+    di = phi_length - d;
+    c1->mu = ((p1.mu >> d) << d) | ((p2.mu << di) >> di);
+    c2->mu = ((p2.mu >> d) << d) | ((p1.mu << di) >> di);
+
+    d = uniform() * (sigma_length - 1) + 1;
+    di = phi_length - d;
+    c1->sigma = ((p1.sigma >> d) << d) | ((p2.sigma << di) >> di);
+    c2->sigma = ((p2.sigma >> d) << d) | ((p1.sigma << di) >> di);
+
+    d = uniform() * (delta_length - 1) + 1;
+    di = phi_length - d;
+    c1->delta = ((p1.delta >> d) << d) | ((p2.delta << di) >> di);
+    c2->delta = ((p2.delta >> d) << d) | ((p1.delta << di) >> di);
+}
+
+void genotype_crossover(const Genotype p1, const Genotype p2, Genotype *const c1, Genotype *const c2) {
+    one_point_crossover(p1, p2, c1, c2);
+}
+
+void bit_flip_mutation(Genotype *const g) {
+    const float prob = 0.5;
+
+    for(int iter = 0; iter < phi_length; iter++) {
+        if(uniform() < prob) {
+            g->phi ^= 1U << iter;
+        }
+    }
+
+    for(int iter = 0; iter < lambda_length; iter++) {
+        if(uniform() < prob) {
+            g->lambda ^= 1U << iter;
+        }
+    }
+
+    for(int iter = 0; iter < mu_length; iter++) {
+        if(uniform() < prob) {
+            g->mu ^= 1U << iter;
+        }
+    }
+
+    for(int iter = 0; iter < sigma_length; iter++) {
+        if(uniform() < prob) {
+            g->sigma ^= 1U << iter;
+        }
+    }
+
+    for(int iter = 0; iter < delta_length; iter++) {
+        if(uniform() < prob) {
+            g->delta ^= 1U << iter;
+        }
+    }
+}
+
+void mutate_genotype(Genotype *const g) {
+    bit_flip_mutation(g);
+}
